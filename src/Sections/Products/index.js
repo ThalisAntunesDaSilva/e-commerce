8329a177e5f4159127bda9df70b8c7538f968b6d
thalisantunes@hotmail.com <thalisantunes@hotmail.com>
2022-05-13 19:47:10 -0300

    feat: link for products

--- src/Sections/Products/index.js
+++ src/Sections/Products/index.js
@@ -1,86 +1,85 @@
 import React from "react";
-import './styles.css';
-import shirt from '../../assets/shirt.jpg'
-import shirt2 from '../../assets/shirt2.jpg'
-import shirt3 from '../../assets/shirt3.jpg'
-
+import "./styles.css";
+import shirt from "../../assets/shirt.jpg";
+import shirt2 from "../../assets/shirt2.jpg";
+import shirt3 from "../../assets/shirt3.jpg";
 
 function Products() {
-  return <div>
+  return (
+    <div>
       <div className="product-release">
-            <h1 className="title-release">Produtos lançados</h1>
-          
-                <ul className="product-list">
-                    <li className="list-item">
-                        <div>
-                            <img src={shirt} className="img-product"/>
-                            <h3 className="title-product">
-                                Camiseta Fashion
-                            </h3>
-                            <h1 className="price-product">R$ 109,90</h1>
-                        </div>
-                        </li>
-                        <li className="list-item">
-                        <div>
-                            <img src={shirt2} className="img-product"/>
-                            <h3 className="title-product">
-                                Camiseta Curta
-                            </h3>
-                            <h1 className="price-product">R$ 109,90</h1>
-                        </div>
-                    </li>
+        <h1 className="title-release">Produtos lançados</h1>
 
-                    <li className="list-item">
-                        <div>
-                            <img src={shirt3} className="img-product"/>
-                            <h3 className="title-product">
-                                Camiseta Xadrez
-                            </h3>
-                            <h1 className="price-product">R$ 109,90</h1>
-                        </div>
-                    </li>
-                   
-                    </ul>
-            
+        <ul className="product-list">
+          <li className="list-item">
+            <a href="/products">
+              <div>
+                <img src={shirt} className="img-product" />
+                <h3 className="title-product">Camiseta Fashion</h3>
+                <h1 className="price-product">R$ 109,90</h1>
+              </div>
+            </a>
+          </li>
+
+          <li className="list-item">
+            <a href="/products">
+              <div>
+                <img src={shirt2} className="img-product" />
+                <h3 className="title-product">Camiseta Curta</h3>
+                <h1 className="price-product">R$ 109,90</h1>
+              </div>
+            </a>
+          </li>
+
+          <li className="list-item">
+            <a href="/products">
+              <div>
+                <img src={shirt3} className="img-product" />
+                <h3 className="title-product">Camiseta Xadrez</h3>
+                <h1 className="price-product">R$ 109,90</h1>
+              </div>
+            </a>
+          </li>
+        </ul>
       </div>
 
       <div className="product-release">
-            <h1 className="title-release">Promoções</h1>
-         
-                <ul className="product-list">
-                    <li className="list-item">
-                        <div>
-                            <img src={shirt} className="img-product"/>
-                            <h3 className="title-product">
-                                Camiseta Fashion
-                            </h3>
-                            <h1 className="price-product">R$ 109,90</h1>
-                        </div>
-                        </li>
-                        <li className="list-item">
-                        <div>
-                            <img src={shirt2} className="img-product"/>
-                            <h3 className="title-product">
-                                Camiseta Curta
-                            </h3>
-                            <h1 className="price-product">R$ 109,90</h1>
-                        </div>
-                    </li>
+        <h1 className="title-release">Promoções</h1>
 
-                    <li className="list-item">
-                        <div>
-                            <img src={shirt3} className="img-product"/>
-                            <h3 className="title-product">
-                                Camiseta Xadrez
-                            </h3>
-                            <h1 className="price-product">R$ 109,90</h1>
-                        </div>
-                    </li>
-                   
-                    </ul>
-            
+        <ul className="product-list">
+          <li className="list-item">
+            <a href="/products"></a>
+            <div>
+              <img src={shirt} className="img-product" />
+              <h3 className="title-product">Camiseta Fashion</h3>
+              <h1 className="price-product">R$ 109,90</h1>
+            </div>
+            <a />
+          </li>
+
+          <li className="list-item">
+            <a href="/products"></a>
+            <div>
+              <img src={shirt2} className="img-product" />
+              <h3 className="title-product">Camiseta Curta</h3>
+              <h1 className="price-product">R$ 109,90</h1>
+            </div>
+            <a />
+          </li>
+
+          <li className="list-item">
+            <a href="/products"></a>
+            <div>
+              <img src={shirt3} className="img-product" />
+              <h3 className="title-product">Camiseta Xadrez</h3>
+              <h1 className="price-product">R$ 109,90</h1>
+            </div>
+            <a />
+          </li>
+        </ul>
       </div>
-  </div>;
+    </div>
+  );
 }
 
 export default Products;
